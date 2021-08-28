c5aaf88b1df4999a0c4e3904a622e0c821672be4
Pedram Karimi <noonegandom@hotmail.com>
2021-08-28 16:06:27 +0430

    Add project files.

---
+++ BunddleDiffCheck/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BunddleDiffCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CollectBundle();
+        }
+
+        private List<string> BundlerList = new List<string>();//bundleconfig.json
+        private List<string> DeveloperList = new List<string>();//index.cs
+    
+    
+        private static void CollectBundle()
+        {
+            fore
+        }
+    
+    
+    }
+}
